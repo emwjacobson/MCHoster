b098b6e161b94566bbb9a0bc9a7da29fec960ac5
Emerson Jacobson <emwjacobson@gmail.com>
2020-09-06 05:45:35 +0000

    Cleaned up some code in using env vars in django settings

--- web/mcweb/settings.py
+++ web/mcweb/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY') if os.environ.get('SECRET_KEY') is not None else ''.join(random.choice(string.printable) for _ in range(64))
+SECRET_KEY = os.environ.get('SECRET_KEY', ''.join(random.choice(string.printable) for _ in range(64)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') if os.environ.get('DEBUG') is not None else False
+DEBUG = bool(os.environ.get('DEBUG', False))
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',') if os.environ.get('ALLOWED_HOSTS') is not None else []
+ENV_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ENV_HOSTS.split(',') if ENV_HOSTS else []
 
 
 # Application definition
