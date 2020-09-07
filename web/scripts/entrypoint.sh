e6bd35273cbc01d139b3caef62db4af53036e7ee
Emerson Jacobson <emwjacobson@gmail.com>
2020-09-07 22:13:47 +0000

    Fixed entrypoint script

--- web/scripts/entrypoint.sh
+++ web/scripts/entrypoint.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-python3 manage.py collectstatic
+echo "yes" | python3 manage.py collectstatic
 
 wait-for-it.sh db:3306
 
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-gunicorn -w 4 -b 0.0.0.0:8000 mcweb.wsgi
+if [ $DEBUG = "True" ]; then
+    python3 manage.py runserver --insecure 0:8000
+else
+    gunicorn -w 4 -b 0.0.0.0:8000 mcweb.wsgi
+fi
