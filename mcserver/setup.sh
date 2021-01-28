51679d5ca71557619279360b7882428cbe08a141
Emerson Jacobson <emwjacobson@gmail.com>
2021-01-28 20:59:25 +0000

    Updated mcserver. Should move to own repo soon.

--- mcserver/setup.sh
+++ mcserver/setup.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
+MAJOR_VERSION=${MC_MAJOR_VERSION}
+DATA=$(curl -s -X GET "https://papermc.io/api/v2/projects/paper/version_group/1.16/builds" -H  "accept: application/json")
+VERSION=$(echo ${DATA} | jq -r '.builds[-1].version')
+BUILD=$(echo ${DATA} | jq -r '.builds[-1].build')
+
+DOWNLOAD_URL="https://papermc.io/api/v2/projects/paper/versions/${VERSION}/builds/${BUILD}/downloads/paper-${VERSION}-${BUILD}.jar"
+
 # Download and accept EULA
 if [ ! -f server.jar ]; then
-    wget ${PAPER_DOWNLOAD_URL} -O server.jar
+    wget ${DOWNLOAD_URL} -O server.jar
     echo "eula=true" > eula.txt
 fi
 
 # Run server
-echo "op ${OP_USERNAME}" | java -Xms${RAM} -Xmx${RAM} -jar server.jar nogui
+cat commands.txt | java -Xms${RAM} -Xmx${RAM} -jar server.jar nogui
